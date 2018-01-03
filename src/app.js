c2014e20a0f8015acdfe7388374340f2f12a23cb
chenm <chenmin@hotmail.de>
2018-01-03 22:22:23 +0800

    栅格

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 Vue.component('wui-button', Button)
 Vue.component('wui-icon', Icon)
 Vue.component('wui-button-group', ButtonGroup)
 Vue.component('wui-input', Input)
+Vue.component('wui-row', Row)
+Vue.component('wui-col', Col)
 
 new Vue({
   el: '#app',
