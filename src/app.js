0134617c81a5229eeaa2eff9091cd13cc616a70a
chenm <chenmin@hotmail.de>
2019-01-04 21:23:23 +0800

    栅格

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
-import Button from './button'
 import Icon from './icon'
-import ButtonGroup from './button-group'
 import Input from './input'
+import Button from './button'
+import ButtonGroup from './button-group'
 import Row from './row'
 import Col from './col'
+import Layout from './layout'
+import Header from './header'
+import Sider from './sider'
+import Content from './content'
+import Footer from './footer'
 
 Vue.component('wui-button', Button)
 Vue.component('wui-icon', Icon)
@@ -12,6 +17,12 @@ Vue.component('wui-button-group', ButtonGroup)
 Vue.component('wui-input', Input)
 Vue.component('wui-row', Row)
 Vue.component('wui-col', Col)
+Vue.component('wui-layout', Layout)
+Vue.component('wui-header', Header)
+Vue.component('wui-content', Content)
+Vue.component('wui-footer', Footer)
+Vue.component('wui-sider', Sider)
+
 
 new Vue({
   el: '#app',
@@ -29,5 +40,6 @@ new Vue({
     }
   }
 })
+
 
 
--- src/row.vue
+++ src/row.vue
@@ -5,7 +5,7 @@
 </template>
 <script>
   export default {
-    name: 'GuluRow',
+    name: 'WUIRow',
     props: {
       gutter: {
         type: [Number, String]
